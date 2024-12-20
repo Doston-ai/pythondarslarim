83bd155ceb1a959bd94394b17ab5839be167e0e5
Doston-ai <ubaydullayevdoston607@gmail.com>
2024-12-20 23:30:33 +0500

    qayta takrorlanuchi fubktsiyalar tugadi

---
+++ functions/function5.py
@@ -0,0 +1,14 @@
+#    Foydalanuvchidan son qabul qilib, shu son miqdoricha Fibonachchi ketma-ketligidagi sonlar ro'yxatni qaytaruvchi funksiya yozing.
+#   Ta’rif: Har bir hadi o’zidan oldingi ikkita hadning yig’indisiga teng bo’lgan ketma-ketlik Fibonachchi ketma-ketligi deyiladi. 
+#   Bunda boshlang’ish had ko’pincha 1 deb olinadi.  1, 1, 2, 3, 5, 8, 13, 21, 34, 55,...
+
+#n = int(input("Son kiriting\n>>>"))
+ketmaketlik = [1,1]
+
+def fibanachi(n):
+    while n>ketmaketlik[-1]:
+        ketmaketlik.append(ketmaketlik[-1]+ketmaketlik[-2])    
+        return fibanachi(n)
+
+fibanachi(10000)
+print(ketmaketlik)
--- functions/functions4.py
+++ functions/functions4.py
@@ -2,11 +2,27 @@
 # (tub sonlar —faqat birga va o'ziga qoldiqsiz bo'linuvchi, 1 dan katta musbat sonlar)
 
 def oraliq(min, max):
-    sonlar  = []
-    while min<max:
-        min += 1
+    sonlar = []
+    while min<=max:
         sonlar.append(min)
-    return sonlar
-min = float(input("Birinchi sonni kiriting: "))
-max = float(input("Oxirgi sonni kiriting: "))
+        min += 1
+    for son in sonlar:
+        
+        return sonlar
+son = oraliq(1,10)
+print(oraliq(1,10))
+
+tubsonlar = []
+for n in son:
+    tub = True
+    if n == 1:
+        tub = False
+    for i in range(2,n):
+        if n%i == 0:
+            tub = False
+    if tub:
+        tubsonlar.append(n)
+print(tubsonlar)
+
+
 
