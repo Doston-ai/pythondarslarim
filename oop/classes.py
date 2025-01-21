83b1432ce18dae0c8db11847b84519651fc2189c
Doston-ai <ubaydullayevdoston607@gmail.com>
2025-01-21 21:35:20 +0500

    python classes

--- oop/classes.py
+++ oop/classes.py
@@ -4,10 +4,26 @@
 
 
 class User:
-    def __init__(self,ism,familiya,foydalanuvchi_ismi,email,):
+    def __init__(self,ism,familiya,yosh,foydalanuvchi_ismi=None,email=None):
 
         self.ism = ism
         self.familiya = familiya
         self.foydalanuvchi_ismi = foydalanuvchi_ismi
         self.email = email
-    user1 = User(Doston)
+        self.yosh = yosh
+    def get_name(self):
+        return self.ism
+        
+    def age(self):
+        return self.yosh
+    def get_info(self):
+        print(f"Foydalunuchi:{self.foydalanuvchi_ismi}, ismi:{self.ism} {self.familiya}, yoshi: {self.yosh}")
+
+user1 = User('Doston','Ubaydullayev',18,'doksbro','ubaydullayevdoston607@gmail.com')
+user2 = User('Diyor','Toyirov',20,'toyirov1@gmail.com')
+
+print(user2.get_info())
+
+
+
+
